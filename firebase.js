01e39387e82ae830a0bc311dbd2bbf7a0855f506
ArfiH <arfihussain27@gmail.com>
2024-09-24 16:50:41 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNbB981Mfdv-rMWExTzLfudOGOTt3A4kI",
+  authDomain: "forflashify.firebaseapp.com",
+  projectId: "forflashify",
+  storageBucket: "forflashify.appspot.com",
+  messagingSenderId: "383183997845",
+  appId: "1:383183997845:web:09ff8b07fb39a8853c049c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
